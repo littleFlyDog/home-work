5d59e51a55e321f66eeedb42fda7c0173d6e0d50
Siyuan Zhang <73239147+littleFlyDog@users.noreply.github.com>
2020-12-08 21:44:33 +0800

    Update charSummary.cpp

--- summary/charSummary.cpp
+++ summary/charSummary.cpp
@@ -1,3 +1,6 @@
+// test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
 #include <iostream>
 using namespace std;
 
@@ -8,7 +11,7 @@ int main()
     //char b;
     //char a[N];
     //一、输入一个字符
-    //c = cin.get();读取一个字符（包括空白字符）后结束，输入一个字符后若其后有回车，不会消耗//
+    //c = cin.get();读取一个字符（包括空白字符）后结束，输入一个字符后若其后有回车，不会消耗，但自身输入的是回车则会消耗//
     //cin>>b;//读取一个字符（不包括空白字符）输入一个非空字符后若其后有回车，不会消耗//
     //故总结：在使用cin.get、cin.getline输入时一定要注意之前是否留存有空白字符！！！！
     //二，输入一行字符串
@@ -31,7 +34,7 @@ int main()
     cin >> n;//这里有一个回车，故下一句lines[0]直接遇到回车结束，这里的回车会被回收消耗
     for (int i = 0; i < n; ++i) cin.getline(lines[i], 80);
     cout << lines[0] << endl << lines[1];*/
-    char a[50];
+    //char a[50];
     /*读入字符串
     一、遇到EOF结束
     1.依次读入一个字符
@@ -39,10 +42,11 @@ int main()
    （2）whlie(char=cin.get()!=EOF)
     两者均会读入空白字符（包括回车）且不停止，除非出现文本的EOF
     2.读入一行字符串*/
-    int len=0;
+    //int len=0;
     //（1）
     //while (cin.get(s[len++]) && s[len] != '\n');
-
+    //char a[50];
+    //int len = 0;
      /*(2)while(s[len++]=cin.get()!=EOF)
      两者均会读入空白字符（包括回车）且不停止，除非出现文本的EOF
      二、遇回车或遇EOF结束
@@ -52,10 +56,11 @@ int main()
      */
      //char a[80]{}; int len = 0;
      //while ((a[len]=cin.get() )&&( a[len] != '\n'))
-     //while(cin.get(a[len])&&a[len]!='\n')//两种皆可
-        // ++len;
+     //while(cin.get(a[len])&&a[len]!='\n')//两种皆可,但是字符串的末尾是'\n',但是可以回收空格
+     
      //for (len = 0; len < 15; ++len)
         // cout << a[len] ;
+    
     return 0;
 
 }
